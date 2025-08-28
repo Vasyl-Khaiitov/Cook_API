1aad32156177039382607ea31b458654feae851c
MazaFaaka <haitovvasyl@gmail.com>
2025-08-28 10:41:57 +0200

    #

--- src/server.js
+++ src/server.js
@@ -23,13 +23,7 @@ export const startServer = () => {
 
   app.use(express.json());
   app.use(cookieParser());
-  app.use(
-    cors({
-      origin: ['*'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      credentials: true,
-    }),
-  );
+  app.use(cors());
   app.use(
     pino({
       transport: {
